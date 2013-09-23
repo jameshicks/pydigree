a232238dd6bf1194dfd83917a38d2e324a91982b
James Hicks <james@motorcyclesarejets.com>
2013-09-23 15:09:51 -0400

    Take out pdb trace from kinship script. Added some extra comments

--- scripts/kinship.py
+++ scripts/kinship.py
@@ -8,9 +8,13 @@ pop = pydigree.Population(5000)
 ped = pydigree.read_ped(sys.argv[1], pop)
 ids = sorted([x.id for x in ped])
 
-import pdb;pdb.set_trace()
+
+# itertools combinations with replacement isn't available in 2.6
+# So we'll do this as a two step process
+# Pairwise kinship coefs
 for x,y in itertools.combinations(ids,2):
     k = pydigree.kinship(ped[x],ped[y])
     print 1,x,y,k
+# Inbreeding coefs.
 for x in ids:
     print 1,x,x,ped[x].inbreeding()
