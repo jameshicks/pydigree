7ab352943c8f2455f13bb43b01708a473886ff2a
James Hicks <james@motorcyclesarejets.com>
2013-09-25 23:59:16 -0400

    makeV function

--- pydigree/mixedmodel/likelihood.py
+++ pydigree/mixedmodel/likelihood.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 
+from itertools import izip
 from math import log as ln
+
 import numpy as np
 from numpy.linalg import inv,pinv,det
 
+def makeV(covariance_mats, variance_components, residual_variance):
+    """
+    Returns the V matrix for the restricted loglikelihood function. For a
+    decent definition, see
+
+    Amos. 'Robust Variance-Components Approach for Assessing Genetic Linkage in Pedigrees'
+    Am J Hum Genet (1994) 54:535-543
+    """
+    V = sum(partial_variance * covariance_matrix \
+            for partial_variance, covariance_matrix in \
+            izip(variance_components, covariance_mats))
+    variance = sum(variance_components) + residual_variance
+    np.fill_diagonal(V,variance)
+    return V
 def restricted_loglikelihood(y,v,x):
     """
     Returns the restricted loglikelihood for mixed model estimation.
