74f387711d6da30706f8680ec21d0c983408f559
James Hicks <james@motorcyclesarejets.com>
2013-09-26 11:06:15 -0400

    references

--- pydigree/mixedmodel/likelihood.py
+++ pydigree/mixedmodel/likelihood.py
@@ -11,8 +11,9 @@ def makeV(covariance_mats, variance_components, residual_variance):
     Returns the V matrix for the restricted loglikelihood function. For a
     decent definition, see
 
-    Amos. 'Robust Variance-Components Approach for Assessing Genetic Linkage in Pedigrees'
-    Am J Hum Genet (1994) 54:535-543
+    Reference:
+    Lange, Westlake, & Spence. Extensions to pedigree analysis III. Variance components by the scoring method.
+    Ann Hum Genet. (1976). 39:4,485-491 DOI: 10.1111/j.1469-1809.1976.tb00156.x
     """
     V = sum(partial_variance * covariance_matrix \
             for partial_variance, covariance_matrix in \
@@ -27,6 +28,9 @@ def restricted_loglikelihood(y,v,x):
     Reference:
     Harville. 'Maximum Likelihood Approaches to Variance Component Estimation and to Related Problems'
     Journal of the American Statistical Association. (1977) (72):258
+
+    Lange, Westlake, & Spence. Extensions to pedigree analysis III. Variance components by the scoring method.
+    Ann Hum Genet. (1976). 39:4,485-491 DOI: 10.1111/j.1469-1809.1976.tb00156.x
     "
     vinv = inv(v)
     p = vinv - vinv * x * pinv(x.T * vinv * x) * x.T * vinv
