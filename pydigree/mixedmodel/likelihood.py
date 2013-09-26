98f168b1401fb92131439b22ea3522b53668c511
James Hicks <james@motorcyclesarejets.com>
2013-09-25 22:21:09 -0400

    reml equation

--- pydigree/mixedmodel/likelihood.py
+++ pydigree/mixedmodel/likelihood.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 
+from math import log as ln
 import numpy as np
+from numpy.linalg import inv,pinv,det
 
+def restricted_loglikelihood(y,v,x):
+    vinv = inv(v)
+    p = vinv - vinv * x * pinv(x.T * vinv * x) * x.T * vinv
+    return - 0.5 * ( ln(det(v) + ln( x.T * vinv * x) + y.T * p * y )
