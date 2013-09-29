843c8976db2fa941d910ee9216885484298ff56c
James Hicks <james@motorcyclesarejets.com>
2013-09-28 21:10:26 -0400

    mixed model class

---
+++ pydigree/mixedmodel/mixedmodel.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+class MixedModel(object):
+    def __init__(self):
+        pass
+    def maximize(self):
+        pass
+    def likelihood(self,metric='REML'):
+        pass
+    def breeding_values(self):
+        pass
+    
