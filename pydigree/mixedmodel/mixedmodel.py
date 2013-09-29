666d4a631ed5a9c73d24b25e6a2cb149df97c759
James Hicks <james@motorcyclesarejets.com>
2013-09-28 21:33:07 -0400

    add makeX method

--- pydigree/mixedmodel/mixedmodel.py
+++ pydigree/mixedmodel/mixedmodel.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 
+import numpy as np
+
 class MixedModel(object):
     def __init__(self):
         pass
+    def _makeX(self):
+        """
+        Builds the design matrix for the fixed effects in the mixed model.
+        Includes a column of ones for the intercept.
+        
+        Arguements: None
+        Returns: A numpy matrix
+        """
+        if len(set(len(x) for x in self.fixed_effects)) > 1:
+            raise ValueError('Not all fixed effects have same number of observations')
+        nobs = len(self.fixed_effects[0])
+        mat = [[1] * nobs] + self.fixed_effects
+        return np.matrix(mat).transpose()
     def maximize(self):
         pass
     def likelihood(self,metric='REML'):
