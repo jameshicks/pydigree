6c4b692a46892acb77c0c2e4829bc19367ed0640
James Hicks <james@motorcyclesarejets.com>
2014-01-08 10:49:24 -0500

    explanatory comments

--- scripts/bitsize.py
+++ scripts/bitsize.py
@@ -3,6 +3,12 @@
 import pydigree
 import sys
 
+# Prints the bit size of each pedigree.
+# The bitsize is defined as 2*n-f where n is the number of nonfounders
+# and f is the number of founders. This represents the number of bits
+# it takes to represent the inheritance vector in the Lander-Green
+# algorithm.
+
 ped = pydigree.read_ped(sys.argv[1])
 
 for pedigree in sorted(ped,key=lambda x: x.label):
