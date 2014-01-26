bfe000f46c04d43c40c258c56ea969a1e0c26385
James Hicks <james@motorcyclesarejets.com>
2014-01-26 15:40:24 -0500

    accept genotype constraints, flip sign on accuracy

--- pydigree/simulation/naivegenedrop.py
+++ pydigree/simulation/naivegenedrop.py
@@ -6,23 +6,27 @@ from simulation import Simulation, SimulationError
 class NaiveGeneDroppingSimulation(Simulation):
     def __init__(self, template=None, replications=1000):
         Simulation.__init__(self, template, replications)
+        self.genedrop_attempts = 1000
 
     def replicate(self):
         self.template.clear_genotypes()
 
-        for i in peds.individuals():
-            if i.is_founder():
-                i.get_genotypes(linkeq=self.linkeq)
-
         for ped in self.template:
+            for founder in ped.founders():
+                if founder in self.constraints['genotype']:
+                    founder.get_constrained_genotypes(self.constraints['genotype'][founder],
+                                                      linkeq=True)
+                else:
+                    founder.get_genotypes()
+
             for attempt in xrange(self.genedrop_attempts):
                 for ind in ped:
                     if not ind.is_founder():
                         ind.clear_genotypes()
                     ind.get_genotypes()
                 if self.trait:
-                    accuracy = self.predicted_trait_accuracy()
-                    if accuracy >= accuracy_threshold:
+                    accuracy = self.predicted_trait_accuracy(ped)
+                    if accuracy < self.accuracy_threshold:
                         continue
                 break
             else:
