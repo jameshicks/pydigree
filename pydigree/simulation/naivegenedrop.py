2e19f4de1e5e206468ae1b6d27421bb898fe25e4
James Hicks <james@motorcyclesarejets.com>
2014-01-27 16:25:19 -0500

    move founder genotype getting into main attempt loop, because otherwise you might run out of simulation attempts

--- pydigree/simulation/naivegenedrop.py
+++ pydigree/simulation/naivegenedrop.py
@@ -9,26 +9,34 @@ class NaiveGeneDroppingSimulation(Simulation):
         self.genedrop_attempts = 1000
 
     def replicate(self):
-        self.template.clear_genotypes()
-
+        for ind in self.template.individuals():
+            ind.clear_genotypes()
         for ped in self.template:
-            for founder in ped.founders():
-                if founder in self.constraints['genotype']:
-                    founder.get_constrained_genotypes(self.constraints['genotype'][founder],
-                                                      linkeq=True)
-                else:
-                    founder.get_genotypes()
 
             for attempt in xrange(self.genedrop_attempts):
+
+                for ind in ped:
+                    ind.clear_genotypes()
+
+                for founder in ped.founders():
+                    if founder in self.constraints['genotype']:
+                        founder.get_constrained_genotypes(self.constraints['genotype'][founder],
+                                                      linkeq=True)
+                    else:
+                        founder.get_genotypes()
+
                 for ind in ped:
                     if not ind.is_founder():
                         ind.clear_genotypes()
+
+                for ind in ped:
                     ind.get_genotypes()
+
                 if self.trait:
                     accuracy = self.predicted_trait_accuracy(ped)
                     if accuracy < self.accuracy_threshold:
                         continue
-                print 'Successful after %s gene dropping attempts' % (attempt)
+                print 'Successful (%s%%) after %s gene dropping attempts' % (accuracy * 100,attempt)
                 break
             else:
                 raise SimulationError('Ran out of gene dropping attempts!')
