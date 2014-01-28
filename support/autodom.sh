57480276cab9a0c675511ce213f70c2bd44b81be
James Hicks <james@motorcyclesarejets.com>
2014-01-28 02:10:37 -0500

    shell script for autosomal dominant simulation

---
+++ support/autodom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Simulate data under an autosomal dominant model
+# Usage: autodom.sh template.ped chromosome.template constraintfile
+
+~/pydigree/scripts/simulate_pedigree_data.py --template $1 \
+    --chromosome $2 --freq 0 19999 0.0 \
+    --method constrained \
+    --constraintfile $3 \
+    --effect 0 19999 2 2 1 \
+    --effect 0 19999 2 1 1 \
+    --liability-threshold 0.5 \
+    --prefix autodom
