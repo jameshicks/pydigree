a8c06db0aa25ec12bc2d01cca5e8660b34dce40d
James Hicks <james@motorcyclesarejets.com>
2014-01-27 23:25:43 -0500

    ibd file generator

---
+++ support/affecteds_ibd_from_founder.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Marks affected individuals as IBD from a common specified ancestor
+# Usage: <script> ped fam ancestor chr pos allele
+
+import sys
+import pydigree
+
+peds = pydigree.read_ped(sys.argv[1])
+fam, ancestor, chr, pos, allele = sys.argv[2:]
+print ' '.join(['GENOTYPE', fam, ancestor, chr, pos, allele, 'P', 'set'])
+
+for ind in peds.individuals():
+    if not ind.phenotypes['affected']:
+        continue
+    print ' '.join(['IBD', fam, ind.id, chr, pos, 'P'])
