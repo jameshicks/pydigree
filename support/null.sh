109e88a583e1909e97b16fd80c9290b8461fc539
James Hicks <james@motorcyclesarejets.com>
2014-02-04 21:59:10 -0500

    support script for null data simulation

---
+++ support/null.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Simulate data under a null model by gene dropping
+
+~/simulations/simulate_pedigree_data.py --template $1 \
+    --chromosome $2 --freq 0 19999 0.0 \
+    --method genedrop \
+    --prefix null \
+    --replications 1000 
