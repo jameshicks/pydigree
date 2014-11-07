7cf7b7db7c03054be5c5b38698a1bb3a250e98c4
James Hicks <james@motorcyclesarejets.com>
2014-11-07 16:12:35 -0600

    less aggressive compression for gzip

--- pydigree/io/smartopen.py
+++ pydigree/io/smartopen.py
@@ -4,7 +4,7 @@ import bz2
 def smartopen(filename, mode='r'):
     "Seamlessly open gzipped and bzipped2 files. Use like regular open"
     if filename.endswith('.gz'):
-        return gzip.GzipFile(filename, mode)
+        return gzip.GzipFile(filename, mode, compresslevel=5)
     elif filename.endswith('.bz2'):
         return bz2.BZ2File(filename, mode)
     else:
