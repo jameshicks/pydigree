3c256308604484818a5b1f54704ba74c8ad9da24
James Hicks <james@motorcyclesarejets.com>
2014-11-07 12:58:28 -0600

    fix path

--- support/null.sh
+++ support/null.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Simulate data under a null model by gene dropping
 
-~/simulations/simulate_pedigree_data.py --template $1 \
+../scripts/simulate_pedigree_data.py --template $1 \
     --chromosome $2 --freq 0 19999 0.0 \
     --method genedrop \
     --prefix null \
