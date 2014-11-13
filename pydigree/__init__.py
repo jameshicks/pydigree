cb120b7b344da448608c041b6ad85a39f74be89f
James Hicks <james@motorcyclesarejets.com>
2014-11-13 16:59:20 -0600

    fix imports

--- pydigree/__init__.py
+++ pydigree/__init__.py
@@ -4,19 +4,26 @@
 from pydigree.common import *
 from pydigree.misc import *
 
-# Reading and writing files
-import pydigree.io
 
 # Functions for navigating pedigree structures
 from pydigree.paths import path_downward, paths, paths_through_ancestor
 from pydigree.paths import common_ancestors, kinship
+
+# Reading and writing files
+import pydigree.io
+
 
 # Population growth models
 from pydigree.population import exponential_growth, logistic_growth
 
 # Classes
 from pydigree.population import Population
+from pydigree.pedigreecollection import PedigreeCollection
 from pydigree.pedigree import Pedigree
 from pydigree.individual import Individual
 from pydigree.chromosome import Chromosome
 from pydigree.mixedmodel import MixedModel
+
+
+# Functions for identifying shared genomic segments (SGS)
+import pydigree.sgs
--- pydigree/sgs/__init__.py
+++ pydigree/sgs/__init__.py
@@ -1,1 +1,2 @@
 from pydigree.sgs.shares import nshares
+from sgs import *
