cde82be42711a1bd3b050b8f0a7f1f801c123792
James Hicks <james@motorcyclesarejets.com>
2014-11-17 00:07:34 -0600

    much faster IBS implementation. runs in 46% of the time

--- pydigree/misc.py
+++ pydigree/misc.py
@@ -2,24 +2,18 @@
 
 
 # Extra genetics functions
-def is_missing_genotype(g):
-    return g == tuple([0, 0])
-
-
 def ibs(g1, g2, checkmissing=True):
     """
     Returns the number of alleles identical by state between two genotypes
     Arguements: Two tuples
     Returns: an integer
     """
-    if checkmissing:
-        if is_missing_genotype(g1) or is_missing_genotype(g2):
-            return None
-    
-    g1 = set(g1)
-    g2 = set(g2)
-    if g1 == g2:
+    a,b,c,d = g1 + g2
+    # Missing genotypes are coded as 0s
+    if 0 in {a,b,c,d}:
+        return None
+    if (a == c and b == d) or (a == d and b == c):
         return 2
-    if g1 & g2:
+    elif a == c or a == d:
         return 1
     return 0
