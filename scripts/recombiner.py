e8364e335dc4d532d18182c8f9df4f996c322c7a
James Hicks <james@motorcyclesarejets.com>
2014-11-18 16:57:15 -0600

    bugfixes

--- scripts/recombiner.py
+++ scripts/recombiner.py
@@ -2,9 +2,11 @@
 
 import random
 import pydigree
+from pydigree.recombination import recombine
 
 nmark = 5000
-poolsize = 1000
+poolsize = 10000
+ngen = 25
 
 c = pydigree.Chromosome()
 for x in range(nmark): c.add_genotype(random.random(),.1)
@@ -13,10 +15,10 @@ mapp = [.1]*nmark
 d =0
 
 print "Initializing pool"
-pool = [c.random_chromosome() for x in range(poolsize)]
+pool = [c.linkageequilibrium_chromosome() for x in range(poolsize)]
 
-for x in range(10):
+for x in xrange(ngen):
     print "Iteration %d" % d
-    recombs = [pydigree.recombine(random.choice(pool),random.choice(pool),mapp)]
+    recombs = [recombine(random.choice(pool),random.choice(pool),mapp) for x in xrange(poolsize)]
     pool = random.sample(recombs+pool,poolsize)
     d += 1
