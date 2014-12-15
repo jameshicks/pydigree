15b4a02b516c0a7a455d72911fc059be2cf9e526
James Hicks <james@motorcyclesarejets.com>
2014-12-15 13:28:31 -0600

    remove fastio reference

--- pydigree/io/__init__.py
+++ pydigree/io/__init__.py
@@ -4,5 +4,4 @@ import pydigree.io.genomesimla
 from pydigree.io.smartopen import smartopen
 
 from pydigree.io.base import read_ped, read_phenotypes
-from pydigree.io.fastio import read_kinship
-from pydigree.io.fastio import read_germline
+
