f229b69227c0d34a9a0bb212f7d2059e2c4d48b4
James Hicks <james@motorcyclesarejets.com>
2014-12-18 15:29:26 -0600

    fix some problems caused by whitespacing

--- pydigree/misc.py
+++ pydigree/misc.py
@@ -1,17 +1,18 @@
 from itertools import izip
 
 
-# Extra genetics functions                                                                                                                                                                                                                        
+# Extra genetics functions
 def ibs(g1, g2, checkmissing=True):
-    """                                                                                                                                                                                                                                           
-    Returns the number of alleles identical by state between two genotypes                                                                                                                                                                        
-    Arguements: Two tuples                                                                                                                                                                                                                        
-    Returns: an integer                                                                                                                                                                                                                           
+    """
+    Returns the number of alleles identical by state between two genotypes
+    Arguements: Two tuples
+    Returns: an integer
     """
     a, b = g1
     c, d = g2
 
-    # Missing genotypes are coded as 0s                                                                                                                                                                                                              if 0 in (a,b,c,d):
+    # Missing genotypes are coded as 0s
+    if 0 in (a,b,c,d):
         return None
     if (a == c and b == d) or (a == d and b == c):
         return 2
