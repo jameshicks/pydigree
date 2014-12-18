1bf921b1c1921982482a73468db947d30ee2181a
James Hicks <james@motorcyclesarejets.com>
2014-12-18 15:25:39 -0600

    bugfix and optimize ibs

--- pydigree/misc.py
+++ pydigree/misc.py
@@ -1,20 +1,21 @@
 from itertools import izip
 
 
-# Extra genetics functions
+# Extra genetics functions                                                                                                                                                                                                                        
 def ibs(g1, g2, checkmissing=True):
+    """                                                                                                                                                                                                                                           
+    Returns the number of alleles identical by state between two genotypes                                                                                                                                                                        
+    Arguements: Two tuples                                                                                                                                                                                                                        
+    Returns: an integer                                                                                                                                                                                                                           
     """
-    Returns the number of alleles identical by state between two genotypes
-    Arguements: Two tuples
-    Returns: an integer
-    """
-    a,b,c,d = g1 + g2
-    # Missing genotypes are coded as 0s
-    if 0 in {a,b,c,d}:
+    a, b = g1
+    c, d = g2
+
+    # Missing genotypes are coded as 0s                                                                                                                                                                                                              if 0 in (a,b,c,d):
         return None
     if (a == c and b == d) or (a == d and b == c):
         return 2
-    elif a == c or a == d:
+    elif a == c or a == d or b == c or b == d:
         return 1
     return 0
 
