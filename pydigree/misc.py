6d5c1f957e565276585366efa34febbe93a6a437
James Hicks <james@motorcyclesarejets.com>
2015-03-31 12:40:40 -0500

    use numpy uint8 dtype

--- pydigree/misc.py
+++ pydigree/misc.py
@@ -33,6 +33,9 @@ def get_ibs_states(ind1, ind2, chromosome_index, missingval=64):
     Returns: A numpy array (dtype: np.uint8) of IBS states, with IBS between
     missing values coded as missingval
     '''
+    if not 0 <= missingval <= 255:
+        raise ValueError('Missing code must be between 0 and 255 inclusive')
+
     a, b = ind1.genotypes[chromosome_index]
     c, d = ind2.genotypes[chromosome_index]
 
@@ -53,7 +56,7 @@ def get_ibs_states(ind1, ind2, chromosome_index, missingval=64):
     # Both alleles are IBS for IBS=2. 
     ibs2 = (a_eq_c & b_eq_d) | (a_eq_d & b_eq_c) 
 
-    ibs_states = np.zeros(a.shape[0])
+    ibs_states = np.zeros(a.shape[0], dtype=np.uint8)
     ibs_states[ibs1] = 1
     ibs_states[ibs2] = 2 
     ibs_states[missing] = missingval
