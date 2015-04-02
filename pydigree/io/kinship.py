4f6b9038dce62e4d13c4aa71701dce0becb3c135
James Hicks <james@motorcyclesarejets.com>
2015-04-02 16:07:08 -0500

    function for reading kinship files

---
+++ pydigree/io/kinship.py
@@ -0,0 +1,7 @@
+def read_kinship(filename):
+    kindict = {}
+    with open(filename) as f:
+        for line in f:
+            fam, ida, idb, phi = line.strip().split()
+            kindict[frozenset({ (fam, ida), (fam, idb)})] = float(phi)
+    return kindict
