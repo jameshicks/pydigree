112d511db78ebb82c405beffb710b38093bf83e5
James Hicks <james@motorcyclesarejets.com>
2015-05-08 13:49:38 -0500

    code cleanup

--- pydigree/cyfuncs.pyx
+++ pydigree/cyfuncs.pyx
@@ -4,29 +4,35 @@ cimport numpy as np
 cimport cython 
 
 cpdef ibs(g1,g2, missingval=None):
- ''' Returns how many alleles are identical-by-state between two genotypes, or missingval if either genotype is missing ''' 
- a,b = g1
- c,d = g2
+    '''
+    Returns how many alleles are identical-by-state between
+    two genotypes, or missingval if either genotype is missing
+    '''
+    a, b = g1
+    c, d = g2
  
- if not (a and b and c and d):
-    return missingval
- if (a == c and b == d) or (a == d and b == c):
-    return 2
- elif a == c or a == d or b == c or b == d:
-    return 1
- return 0
+    if not (a and b and c and d):
+        return missingval
+    if (a == c and b == d) or (a == d and b == c):
+        return 2
+    elif a == c or a == d or b == c or b == d:
+        return 1
+    return 0
 
 
 def runs(sequence, predicate, minlength=2):
     """
-    Identifies runs of values for which predicate(value) evaluates True and yields 2-tuples of the start and end (inclusive) indices
+    Identifies runs of values for which predicate(value) evaluates True
+    and yields 2-tuples of the start and end (inclusive) indices
     """
     cdef int inrun = False
     cdef int start, stop
+    
     for i,v in enumerate(sequence):
         if not inrun and predicate(v):
             inrun = True
             start = i
+    
         elif inrun and not predicate(v):
             inrun = False
             stop = i - 1
@@ -37,6 +43,7 @@ def runs(sequence, predicate, minlength=2):
         stop = i
         if stop - start >= minlength:
             yield start, stop
+
     
 def runs_gte(sequence, double minval, int minlength=2):
     cdef int inrun = False
@@ -57,6 +64,7 @@ def runs_gte(sequence, double minval, int minlength=2):
     if inrun and (i - start) >= minlength:
         out.append((start, i))
     return out
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -79,6 +87,7 @@ def runs_gte_uint8(np.ndarray[np.uint8_t] sequence, np.uint8_t minval, Py_ssize_
     if inrun and (i - start) >= minlength:
         out.append((start, i))
     return out
+
 
 def set_intervals_to_value(intervals, size, value):
     DTYPE = np.int
