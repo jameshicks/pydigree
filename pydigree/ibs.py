1769f04fd6cb06d0e100cae9a35808f1577ccb1e
James Hicks <james@motorcyclesarejets.com>
2015-05-08 13:34:47 -0500

    remove python versions of functions

--- pydigree/ibs.py
+++ pydigree/ibs.py
@@ -4,26 +4,6 @@ import numpy as np
 from pydigree.cyfuncs import ibs
 
 
-# Extra genetics functions
-def py_ibs(g1, g2, checkmissing=True):
-    """
-    Returns the number of alleles identical by state between two genotypes
-    Arguements: Two tuples
-    Returns: an integer
-    """
-    a, b = g1
-    c, d = g2
-
-    # Missing genotypes are coded as 0s
-    if not (a and b and c and d):
-        return None
-    if (a == c and b == d) or (a == d and b == c):
-        return 2
-    elif a == c or a == d or b == c or b == d:
-        return 1
-    return 0
-
-
 def get_ibs_states(ind1, ind2, chromosome_index, missingval=64):
     '''
     Efficiently returns IBS states across an entire chromsome.
@@ -65,8 +45,3 @@ def chromwide_ibs(a, b, c, d, missingval=64)
 
     return ibs_states
 
-
-def py_get_ibs_states(ind1, ind2, chromosome_index):
-    genos1 = izip(*ind1.genotypes[chromosome_index])
-    genos2 = izip(*ind2.genotypes[chromosome_index])
-    return [ibs(x, y) for x, y in izip(genos1, genos2)]
