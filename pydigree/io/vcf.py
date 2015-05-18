e5358210e6d4ddf46c0e36fde010560e3a33fce8
James Hicks <james@motorcyclesarejets.com>
2015-05-18 16:02:29 -0500

    go back to splitting on whitespace by default

--- pydigree/io/vcf.py
+++ pydigree/io/vcf.py
@@ -24,7 +24,7 @@ class VCFRecord(object):
         self.filter_passed = filter_passed == 'PASS'
         self.info = info
         self.format = format
-        self.data = data.split('\t')
+        self.data = data.split()
 
     def genotypes(self, minqual=20, mindepth=8):
         ''' Extract the genotypes from a VCF record '''
