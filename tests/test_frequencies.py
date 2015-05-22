29872da462ef479d4dae13db58af1de57b482e48
James Hicks <james@motorcyclesarejets.com>
2015-05-22 14:28:27 -0500

    more frequency functions

--- tests/test_frequencies.py
+++ tests/test_frequencies.py
@@ -1,5 +1,8 @@
+from __future__ import division
+
 from nose import with_setup
 from pydigree import Population, Individual
+from pydigree.genotypes import Alleles
 
 def setup_80freq():
     pop = Population()
@@ -11,12 +14,40 @@ def setup_80freq():
         ind.genotypes = [ [ [2],[1] ] ]
     return pop
 
+def test_allele_list():
+    pop = Population()
+    loc = 0,0
+    # 2000 A alleles
+    for x in xrange(1000):
+        ind = pop.founder_individual()
+        ind.genotypes = [ (Alleles([1]), Alleles([1]) )]
+    # 500 A alleles, 500 B
+    for x in xrange(500):
+        ind = pop.founder_individual()
+        ind.genotypes = [ (Alleles([1]), Alleles([2]) )]
+    assert sorted(pop.allele_list(loc)) == [1]*2500 + [2]*500
+
+def test_major_allele():
+    pop = Population()
+
+    # 2000 A alleles
+    for x in xrange(1000):
+        ind = pop.founder_individual()
+        ind.genotypes = [ (Alleles([1]), Alleles([1]) )]
+    # 500 A alleles, 500 B
+    for x in xrange(500):
+        ind = pop.founder_individual()
+        ind.genotypes = [ (Alleles([1]), Alleles([2]) )]
+    assert pop.major_allele((0,0)) == 1
+
+
 def test_freq():
     pop = setup_80freq()
     loc = 0,0
     assert len(pop.allele_list(loc)) == 2 * 1000
     assert pop.allele_frequency(loc, 1) == ((2*600) + (1*400)) / float(2*1000)
     assert pop.allele_frequency(loc, 2) == (1*400) / float(2*1000)
+
 
 def test_ld():
     pass
