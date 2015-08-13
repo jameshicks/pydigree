848f61a9962bf90a8a28e3a0d863c246722c5c52
James Hicks <james@motorcyclesarejets.com>
2015-08-13 14:09:13 -0500

    pep8

--- pydigree/sgs/sgs.py
+++ pydigree/sgs/sgs.py
@@ -11,7 +11,8 @@ from pydigree import Population, PedigreeCollection
 
 
 class Segment(object):
-    __slots__ = ['ind1', 'ind2', 'chromosome', 'start', 'stop', '_chridx']
+    __slots__ = ['ind1', 'ind2', 'chromosome', 'start', 'stop',
+                 '_chridx', 'physical_location', 'genetic_location']
 
     def __init__(self, ind1, ind2, chromobj, startidx, stopidx):
         self.ind1 = ind1
@@ -51,10 +52,10 @@ class Segment(object):
     @property
     def missing(self):
         chridx = self._chridx
-        miss1 = ind1.genotypes[chridx][
-            0].missing | ind1.genotypes[chridx][1].missing
-        miss2 = ind2.genotypes[chridx][
-            0].missing | ind2.genotypes[chridx][1].missing
+        miss1 = (ind1.genotypes[chridx][0].missing |
+                 ind1.genotypes[chridx][1].missing)
+        miss2 = (ind2.genotypes[chridx][0].missing |
+                 ind2.genotypes[chridx][1].missing)
         miss = miss1 | miss2
         return miss[self.start:(self.stop + 1)]
 
@@ -65,7 +66,9 @@ class Segment(object):
 
 
 def sgs_pedigrees(pc, phaseknown=False):
-    ''' Performs within-pedigree SGS for each pedigree in a pedigree collection '''
+    '''
+    Performs within-pedigree SGS for each pedigree in a pedigree collection
+    '''
     shared = {}
     for pedigree in pedigrees:
         shared[pedigree] = sgs_population(pedigree)
@@ -83,20 +86,29 @@ def sgs_population(pop, seed_size=500, phaseknown=False, min_length=1,
         shared[pair] = []
         for chridx, chromosome in enumerate(ind1.chromosomes):
             shares = sgs_unphased(ind1, ind2, chridx, seed_size=seed_size,
-                                  min_length=min_length, size_unit=size_unit, min_density=min_density, maxmiss=0.25)
+                                  min_length=min_length, size_unit=size_unit,
+                                  min_density=min_density, maxmiss=0.25)
             shared[pair].append(shares)
     return shared
 
-def sgs_autozygous(ind, chromosome_idx, seed_size=500, phaseknown=False, min_length=1,
-                   size_unit='mb', min_density=100, maxmiss=0.25):
-    chromosome = ind.chromosomes[chromosome_idx] 
+
+def sgs_autozygous(ind, chromosome_idx, seed_size=500,
+                   phaseknown=False, min_length=1, size_unit='mb',
+                   min_density=100, maxmiss=0.25):
+    chromosome = ind.chromosomes[chromosome_idx]
     hapa, hapb = ind.genotypes[chromosome_idx]
     homozygous = (hapa == hapb).astype(np.uint8)
-    autozygous_segs = list(_process_segments(homozygous, min_seg=seed_size,
-                                       min_val=1, chromobj=chromosome,
-                                       min_length=min_length, size_unit=size_unit,
-                                       min_density=min_density, maxmiss=0.25))
-    return [Segment(ind, ind, chromosome, start, stop) for start, stop in autozygous_segs]
+    autozygous_segs = list(_process_segments(homozygous,
+                                             min_seg=seed_size,
+                                             min_val=1,
+                                             chromobj=chromosome,
+                                             min_length=min_length,
+                                             size_unit=size_unit,
+                                             min_density=min_density,
+                                             maxmiss=0.25))
+    return [Segment(ind, ind, chromosome, start, stop)
+            for start, stop in autozygous_segs]
+
 
 def sgs_unphased(ind1, ind2, chromosome_idx, seed_size=255,
                  min_length=1, size_unit='mb', min_density=100,
@@ -110,15 +122,19 @@ def sgs_unphased(ind1, ind2, chromosome_idx, seed_size=255,
     # First get the segments that are IBD=1
     ibd1_segs = list(_process_segments(identical, min_seg=seed_size,
                                        min_val=1, chromobj=chromosome,
-                                       min_length=min_length, size_unit=size_unit,
-                                       min_density=min_density, maxmiss=0.25))
+                                       min_length=min_length,
+                                       size_unit=size_unit,
+                                       min_density=min_density,
+                                       maxmiss=0.25))
     ibd1 = set_intervals_to_value(ibd1_segs, nmark, 1)
 
     # Then the segments that are IBD=2
     ibd2_segs = list(_process_segments(identical, min_seg=seed_size,
                                        min_val=2, chromobj=chromosome,
-                                       min_length=min_length, size_unit=size_unit,
-                                       min_density=min_density, maxmiss=0.25))
+                                       min_length=min_length,
+                                       size_unit=size_unit,
+                                       min_density=min_density,
+                                       maxmiss=0.25))
     ibd2 = set_intervals_to_value(ibd2_segs, nmark, 2)
     ibd = np.maximum(ibd1, ibd2, dtype=np.uint8)
     if array:
@@ -131,7 +147,8 @@ def sgs_unphased(ind1, ind2, chromosome_idx, seed_size=255,
 
 
 def _process_segments(identical, min_seg=100, min_val=1, chromobj=None,
-                      min_density=100, size_unit='mb', min_length=1, maxmiss=0.25):
+                      min_density=100, size_unit='mb',
+                      min_length=1, maxmiss=0.25):
     # IBD segments are long runs of identical genotypes
     ibd = runs_gte_uint8(identical, min_val, minlength=min_seg)
 
@@ -144,8 +161,11 @@ def _process_segments(identical, min_seg=100, min_val=1, chromobj=None,
     ibd = join_gaps(ibd, max_gap=2)
 
     if chromobj:
-        ibd = filter_segments(chromobj, ibd, identical, min_length=min_length,
-                              size_unit=size_unit, min_density=min_density, maxmiss=0.25)
+        ibd = filter_segments(chromobj, ibd, identical,
+                              min_length=min_length,
+                              size_unit=size_unit,
+                              min_density=min_density,
+                              maxmiss=0.25)
 
     return ibd
 
@@ -175,7 +195,9 @@ def filter_segments(chromosome, intervals, identical, min_length=1.0,
         size = locations[stop] - locations[start]
         density = nmarkers / float(size)
         missrate = missing[start:stop].sum() / float(nmarkers)
-        return size >= min_length and density >= min_density and missrate <= maxmiss
+        return (size >= min_length and
+                density >= min_density and
+                missrate <= maxmiss)
 
     return [seg for seg in intervals if meets_criteria(seg)]
 
@@ -199,6 +221,8 @@ def ibd_matrix(shared, individuals, locus):
     return mat
 
 # Support functions
+
+
 def join_gaps(seq, max_gap=1):
     seq = list(seq)
 
@@ -223,10 +247,12 @@ def join_gaps(seq, max_gap=1):
 def make_intervals(ibdarray):
     ibdarray = np.array(ibdarray, dtype=np.uint8)
     ibdarray = ibdarray.copy()
+
     # Get the intervals that are IBD=2 and remove them from the array
     ibd2_tracts = [x for x in runs_gte_uint8(ibdarray, 2)]
     for start, stop in ibd2_tracts:
         ibdarray[start:(stop + 1)] -= 1
+
     # Now get the remaining IBD=1 tracts and remove them from the array
     ibd1_tracts = [x for x in runs_gte_uint8(ibdarray, 1)]
     for start, stop in ibd1_tracts:
