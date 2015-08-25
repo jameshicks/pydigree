e70575825c143cf10f89bad502ae4ad2f6dbf4ca
James Hicks <james@motorcyclesarejets.com>
2015-08-25 03:07:27 -0500

    code clean

--- tests/test_chromatid_labeler.py
+++ tests/test_chromatid_labeler.py
@@ -1,6 +1,7 @@
 from pydigree import Individual, Population
 from pydigree.genotypes import ChromosomeTemplate, Alleles
-from pydigree.genotypes import chromatid_delabeler, AncestralAllele
+from pydigree.genotypes import chromatid_delabeler
+from pydigree.genotypes import AncestralAllele
 
 def test_labeler():
     p = Population()
@@ -18,25 +19,30 @@ def test_labeler():
     assert all(ind.genotypes[0][1] == hapB)
 
 def test_delabeler():
+    ngenos = 10 # Number of genotypes per chromosome
+    if ngenos % 2 == 1: raise ValueError('Even number of genotypes needed')
+
     p = Population()
     c = ChromosomeTemplate()
-    for i in xrange(10):
+    for i in xrange(ngenos):
         c.add_genotype()
     p.add_chromosome(c)
 
+
     a = Individual(p, 1)
     a._init_genotypes(blankchroms=False)
-    a.genotypes[0][0] = Alleles([1]*10)
-    a.genotypes[0][1] = Alleles([2]*10)
+    a.genotypes[0][0] = Alleles([1]*ngenos)
+    a.genotypes[0][1] = Alleles([2]*ngenos)
 
     b = Individual(p, 2)
     b._init_genotypes(blankchroms=False)
-    b.genotypes[0][0] = Alleles([3] * 10)
-    b.genotypes[0][1] = Alleles([4] * 10)
+    b.genotypes[0][0] = Alleles([3] * ngenos)
+    b.genotypes[0][1] = Alleles([4] * ngenos)
 
-    chromatid = [AncestralAllele(a,0)] * 5 + [AncestralAllele(b,1)] * 5
+    chromatid = [AncestralAllele(a,0)] * (ngenos/2) + [AncestralAllele(b,1)] * (ngenos/2)
     chromatid = Alleles(chromatid)
     expected_result = Alleles([1,1,1,1,1,4,4,4,4,4])
+    assert len(expected_result) == ngenos # Sanity check    
     result = chromatid_delabeler(chromatid, 0)
     assert all(result == expected_result)
     assert isinstance(result, Alleles)
