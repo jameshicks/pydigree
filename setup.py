ab3824db1542e20771ef32a73b6b8e5be8204d60
James Hicks <james@motorcyclesarejets.com>
2015-08-26 16:25:31 -0500

    make it easier to profile cython functions when i need to

--- setup.py
+++ setup.py
@@ -12,26 +12,36 @@ from distutils.log import warn, error
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
-import numpy 
+cyprofile = False
+if cyprofile:
+    from Cython.Compiler.Options import directive_defaults
+    directive_defaults['linetrace'] = True
+    directive_defaults['binding'] = True
 
+import numpy
 
 
 c_ext = Extension(
     "pydigree._pydigree",
     ["pydigree/_pydigree.c"],
     include_dirs=[numpy.get_include()]
-    )
+)
 
 cysources = ['pydigree/cyfuncs.pyx']
 if not all(os.path.exists(x) for x in cysources):
-    error('ERROR: Sources not found! Giving up.')
+    error('ERROR: Cython sources not found! Giving up.')
     exit(1)
 
+if cyprofile:
+    macros = [('CYTHON_TRACE', '1')]
+else:
+    macros = None
 
-cyext = [Extension('pydigree.cyfuncs', cysources, include_dirs=[numpy.get_include()])]
+cyext = [Extension('pydigree.cyfuncs', cysources,
+                   include_dirs=[numpy.get_include()], define_macros=macros)]
 
 setup(
     packages=['pydigree'],
     ext_modules=[c_ext] + cythonize(cyext),
     requires=['numpy', 'scipy']
-    )
+)
