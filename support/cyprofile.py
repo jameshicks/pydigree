a7056aa6cbcb530c094a1b704cd849453fe78323
James Hicks <james@motorcyclesarejets.com>
2015-08-26 16:39:01 -0500

    a blueprint for profiling cython functions

---
+++ support/cyprofile.py
@@ -0,0 +1,18 @@
+from pydigree.common import spans
+from pydigree.genotypes import Alleles
+try:
+    import line_profiler
+except ImportError:
+    print("No line profiler, skipping test.")
+    import sys
+    sys.exit(0)
+
+test_data = Alleles([0] * 10000)
+func = spans
+
+for start in xrange(1, 10000, 200):
+	test_data[start:(start+100)] = 1
+
+profile = line_profiler.LineProfiler(func)
+profile.runcall(func, test_data)
+profile.print_stats()
