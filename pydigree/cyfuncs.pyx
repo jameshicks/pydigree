b1b4f619f274dfaa7747fb1195bfe535de950f82
James Hicks <james@motorcyclesarejets.com>
2015-08-26 23:05:03 -0500

    one more type

--- pydigree/cyfuncs.pyx
+++ pydigree/cyfuncs.pyx
@@ -118,7 +118,7 @@ def fastfirstitem(tuple2d):
 def spans(iter):
     cdef  Py_ssize_t start, stop
     cdef  Py_ssize_t itersize, i
-    cdef object item
+    cdef object item, lastitem
 
     itersize = len(iter)
     cdef list identified = []
@@ -130,7 +130,6 @@ def spans(iter):
     lastitem = iter[0]
     for i in range(1, itersize):
         item = iter[i]
-        
         if item != lastitem:
             stop = i 
             tup = lastitem, start, stop
