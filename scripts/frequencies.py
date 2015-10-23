169c339e00f1bfdcf6bd705e099e5ece0111153b
James Hicks <james@motorcyclesarejets.com>
2015-10-22 23:50:48 -0500

    optionally work on a subset of snps

--- scripts/frequencies.py
+++ scripts/frequencies.py
@@ -5,14 +5,24 @@ import pydigree as pyd
 parser = argparse.ArgumentParser()
 parser.add_argument('--ped', required=True, help='Plink formatted PED file')
 parser.add_argument('--map', required=True, help='Plink formatted MAP file')
+parser.add_argument('--snps', required=None, nargs='*', metavar='SNP', 
+                    default=None, 
+                    help='Only calculate frequencies for the specified SNPS')
 args = parser.parse_args()
 
 peds = pyd.io.plink.read_plink(pedfile=args.ped, mapfile=args.map)
 
+if args.snps is not None:
+    onlysnps = set(args.snps)
+
 def formatted(*cells):
     return '\t'.join([str(x) for x in cells])
+
 for chromidx, chromobj in enumerate(peds.chromosomes):
     for locidx, markername in enumerate(chromobj.labels):
+        if args.snps is not None and markername not in onlysnps:
+            continue
+
         locus = chromidx, locidx
         freqs = peds.allele_frequencies(locus).items()
         freqs = sorted(freqs, key=lambda x: x[1], reverse=True)
@@ -22,3 +32,4 @@ for chromidx, chromobj in enumerate(peds.chromosomes):
             print formatted(chromobj.label, chromobj.physical_map[locidx],
                             chromobj.labels[locidx], maj_allele, min_allele,
                             maf_str)
+
