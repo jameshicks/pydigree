8035c0a7465ba35ea58dd0f0b88b154553a2510a
James Hicks <james@motorcyclesarejets.com>
2015-11-12 16:05:19 -0600

    convenience module for RNG-based functions

---
+++ pydigree/rand.py
@@ -0,0 +1,11 @@
+import random
+import numpy as np
+
+def choice(iter):
+	l = len(iter)
+	idx = np.random.randint(0, l)
+	return iter[idx]
+
+def sample_with_replacement(seq, n):
+	l = len(seq)
+	return [seq[np.random.randint(0,l)] for x in xrange(n)]
--- support/recombiner.py
+++ support/recombiner.py
@@ -4,22 +4,28 @@ import numpy as np
 
 from pydigree import ChromosomeTemplate
 from pydigree.recombination import recombine
+from pydigree.rand import choice as randchoice
+from pydigree.rand import sample_with_replacement
 
 nmark = 5000
 poolsize = 10000
 ngen = 25
 
 c = ChromosomeTemplate()
-for x in range(nmark): c.add_genotype(np.random.random(),.1)
+for x in range(nmark):
+    c.add_genotype(np.random.random(), .1)
 mapp = [.1]*nmark
 
-d = 0
+
 
 print "Initializing pool"
-pool = [c.linkageequilibrium_chromosome() for x in range(poolsize)]
+pool = [c.linkageequilibrium_chromosome(sparse=True) for x in range(poolsize)]
 
 for x in xrange(ngen):
-    print "Iteration %d" % d
-    recombs = [recombine(np.random.choice(pool),np.random.choice(pool),mapp) for x in xrange(poolsize)]
-    pool = np.random.choice(recombs+pool,poolsize)
-    d += 1
+    print "Iteration {}".format(x)
+    recombs = [recombine(randchoice(pool),
+                         randchoice(pool),
+                         mapp)
+               for x in xrange(poolsize)]
+    pool = sample_with_replacement(recombs+pool, poolsize)
+
