cb54103ee8825e00d9a723a28c0493505aeb26da
James Hicks <james@motorcyclesarejets.com>
2015-12-15 14:48:21 -0600

    fixes

--- pydigree/datastructures.py
+++ pydigree/datastructures.py
@@ -1,6 +1,8 @@
 import bisect
 
+
 class SortedPairContainer(object):
+
     def __init__(self, pairs):
         self.container = list(pairs)
 
@@ -16,6 +18,13 @@ class SortedPairContainer(object):
         f = bisect.bisect_left(self.indices, key)
         return self.indices[f] == key
 
+    def __delitem__(self, key):
+        if not key in self:
+            raise KeyError
+        internal_idx = self.getindex(key)
+
+        del self.container[internal_idx]
+
     @property
     def indices(self):
         return [x[0] for x in self.container]
@@ -25,22 +34,34 @@ class SortedPairContainer(object):
         Returns the closest index in the container to the index in the full set
         after: Return the index after the last index found, for slicing 
         '''
-        return bisect.bisect(self.indices, full_idx) + (1 if after else 0)
+
+        # Locate the leftmost value exactly equal to x
+        # From the python standard lib:
+        # https://docs.python.org/2/library/bisect.html
+
+        i = bisect.bisect_left(self.indices, full_idx)
+        if i != len(self.container) and self.indices[i] == full_idx:
+            return i
+        raise KeyError
 
     def _getloc(self, key):
-        idx = bisect.bisect_left(self.indices, key)
-
-        if self.container[idx][0] != key:
-            raise KeyError
-        else:
-            return self.container[idx][1]
+        idx = self.getindex(key)  
+        return self.container[idx][1]
 
     def _getslice(self, key):
         ''' Returns a list of pairs within the slice '''
         if key.step is not None:
             raise NotImplementedError
 
-        start = self.getindex(key.start)
-        stop = self.getindex(key.stop)
-        print start,stop
+        indices = self.indices
+        start = self._find_index_low(key.start)
+        stop = self._find_index_high(key.stop)  
+        print start, stop
         return self.container[start:stop]
+
+    def _find_index_low(self, key):
+        "Find the leftmost index less than or equal to key"
+        return bisect.bisect_left(self.indices, key)
+    def _find_index_high(self, key):
+        "Find the rightmost index gte to key"
+        return bisect.bisect_right(self.indices, key)
--- tests/test_paircontainer.py
+++ tests/test_paircontainer.py
@@ -1,16 +1,17 @@
 
-def test_sparsecontainer_slice():
+def maketestdata():
 	from pydigree.datastructures import SortedPairContainer
 	testdata = [(1,'a'), (5, 'b'), (6,'c'), (9,'d')]
 	t =  SortedPairContainer(testdata)
+	return t
 
+def test_sparsecontainer_slice():
+	t = maketestdata()
 	assert t[4:6] == [(5,'b'), (6,'c')]
 
 def test_sparsecontainer_get():
-	from pydigree.datastructures import SortedPairContainer
 	from nose.tools import assert_raises
-	testdata = [(1,'a'), (5, 'b'), (6,'c'), (9,'d')]
-	t = SortedPairContainer(testdata)
+	t = maketestdata()
 	assert t[5] == 'b'
 
 	def should_raise_keyerror(idx):
@@ -19,8 +20,17 @@ def test_sparsecontainer_get():
 	assert_raises(KeyError, should_raise_keyerror, 4)
 	
 def test_sparsecontainer_contains():
-	from pydigree.datastructures import SortedPairContainer
-	testdata = [(1,'a'), (5, 'b'), (6,'c'), (9,'d')]
-	t = SortedPairContainer(testdata)
+	t = maketestdata()
 	assert 5 in t
 	assert 2 not in t
+
+def test_sparsecontainer_getindex(): 
+	t = maketestdata()
+	assert t.getindex(5) == 1
+
+def test_sparsecontainer_delitem():
+	t = maketestdata()
+
+	assert 5 in t
+	del t[5]
+	assert 5 not in t
