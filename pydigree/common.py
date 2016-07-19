7276cb459c7a9612ee503093004a80f20bc6b252
James Hicks <james@motorcyclesarejets.com>
2016-07-18 19:00:49 -0500

    is_sorted method

--- pydigree/common.py
+++ pydigree/common.py
@@ -4,7 +4,7 @@ from itertools import izip_longest
 from operator import mul as multiply
 from math import log
 from pydigree.cyfuncs import runs, runs_gte, interleave
-from pydigree.cyfuncs import all_same_type
+from pydigree.cyfuncs import all_same_type, is_sorted
 
 
 def count(val, iterable):
