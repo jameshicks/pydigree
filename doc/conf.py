cd637b6c7f653526ee4aac07fedc98ea53e7d04b
James Hicks <james@motorcyclesarejets.com>
2017-01-20 12:50:34 -0600

    change doc html theme

--- doc/conf.py
+++ doc/conf.py
@@ -23,6 +23,7 @@
 
 
 # -- General configuration ------------------------------------------------
+import sphinx_rtd_theme
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
@@ -84,8 +85,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = sphinx_rtd_theme.get_html_theme_path()
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
