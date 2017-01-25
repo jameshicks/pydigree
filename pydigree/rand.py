19ead354f334077edce70e4bce50dca678d5d7fe
James Hicks <james@motorcyclesarejets.com>
2017-01-25 14:24:51 -0600

    code cleanup

--- pydigree/rand.py
+++ pydigree/rand.py
@@ -1,10 +1,27 @@
+"Convenience functions for randomness"
+
 import numpy as np
 
-def choice(iter):
-	l = len(iter)
-	idx = np.random.randint(0, l)
-	return iter[idx]
+def choice(seq):
+    """
+    Randomly chooses an item from a sequence. 
+    Probabilities are uniform.
+
+    :param seq: choices
+    :returns: randomly chosen item
+    """
+    l = len(seq)
+    idx = np.random.randint(0, l)
+    return seq[idx]
 
 def sample_with_replacement(seq, n):
-	l = len(seq)
-	return [seq[np.random.randint(0,l)] for x in range(n)]
+    """
+    Choose a sample of n items with replacement from a sequence c
+
+    :param seq: sequence to choose from
+    :param n: sample size
+    :returns: randomly chosen items
+    :rtype: list
+    """
+    l = len(seq)
+    return [seq[np.random.randint(0,l)] for x in range(n)]
--- pydigree/stats/stattests.py
+++ pydigree/stats/stattests.py
@@ -1,30 +1,56 @@
+"Methods for statistical testing"
+
 from math import log
 from scipy import stats 
 
 def LikelihoodRatioTest(null_model, alt_model):
-	"""
-	Compares two nested models by likelihood ratio test
+    """
+    Compares two nested models by likelihood ratio test
 
-	:returns: Result of test
-	:rtype: LikelhoodRatioTestResult
-	"""
+    :returns: Result of test
+    :rtype: LikelhoodRatioTestResult
+    """
 
-	chisq = -2.0 * null_model.loglikelihood() + 2.0 * alt_model.loglikelihood()
-	df = null_model.df - alt_model.df # Null model has more DFs
-	res = LikelihoodRatioTestResult(chisq, df, stats.chi2, len(null_model.observations()))
-	return res
+    chisq = -2.0 * null_model.loglikelihood() + 2.0 * alt_model.loglikelihood()
+    df = null_model.df - alt_model.df # Null model has more DFs
+    res = LikelihoodRatioTestResult(chisq, df, stats.chi2, len(null_model.observations()))
+    return res
 
 class LikelihoodRatioTestResult(object):
-	def __init__(self, statistic, df, distribution, n):
-		self.statistic = statistic
-		self.df = df
-		self.distribution = distribution
-		self.n = n
-	
-	@property
-	def pvalue(self):
-		return self.distribution.sf(self.statistic, self.df)
+    """
+    The result of a likelihood ratio test.
 
-	@property
-	def lod(self):
-		return self.statistic / (2.0 * log(10.0))
+    :ivar statistic: test statistic
+    :ivar df: degrees of freedom
+    :ivar distribution: distribution of test statistic
+    :ivar n: sample size
+
+    :type statistic: float
+    :type df: int
+    :type distribution: scipy probability distribution
+    :type n: int
+    """
+
+    def __init__(self, statistic, df, distribution, n):
+        self.statistic = statistic
+        self.df = df
+        self.distribution = distribution
+        self.n = n
+    
+    @property
+    def pvalue(self):
+        """
+        P-value for the test
+
+        :rtype: float
+        """
+        return self.distribution.sf(self.statistic, self.df)
+
+    @property
+    def lod(self):
+        """
+        LOD score (log10 LR) of the result
+
+        :rtype float:
+        """
+        return self.statistic / (2.0 * log(10.0))
